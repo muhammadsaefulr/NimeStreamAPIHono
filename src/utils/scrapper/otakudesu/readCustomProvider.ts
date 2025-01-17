5b3d6e281673fc46016bf34053705f7442c1c33f
muhammadsaefulr <saepull.id@gmail.com>
2025-01-17 10:51:00 +0700

    update scrapping using user agent

--- src/utils/scrapper/otakudesu/readCustomProvider.ts
+++ src/utils/scrapper/otakudesu/readCustomProvider.ts
@@ -3,12 +3,28 @@ import mainService, {
   pdrainExtractorServicePromise,
 } from "service/mainService/mainService";
 import PdrainExtractorService from "./PdreainExtractor";
+import axios from "axios";
 
 const url = "https://otakudesu.cloud/";
+const headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+  'Accept-Language': 'en-US,en;q=0.9',
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Connection': 'keep-alive',
+  'Upgrade-Insecure-Requests': '1',
+  'Sec-Fetch-Dest': 'document',
+  'Sec-Fetch-Mode': 'navigate',
+  'Sec-Fetch-Site': 'none',
+  'Sec-Fetch-User': '?1',
+  'Cache-Control': 'max-age=0',
+};
+
+
 class WebScraperOtakudesu {
   private static async fetchHtml(url: string): Promise<string> {
-    const response = await fetch(url);
-    return await response.text();
+    const response = await axios.get(url, { headers });
+    return await response.data;
   }
 
   private static async cheerioInstance(pathUri: string): Promise<CheerioAPI> {
@@ -191,6 +207,7 @@ class WebScraperOtakudesu {
 
   static async scrapeVideoAnimeSource(pathUri: string): Promise<any> {
     const $ = await this.cheerioInstance(pathUri);
+    console.log($, pathUri)
     let data = {};
     let AnimeSource: any[] = [];
     let epsList: any[] = [];
